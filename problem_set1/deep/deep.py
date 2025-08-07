c468c6febb7edeff675251c43e44b520d169fc28
git <kanekarayush777@gmail.com>
2025-08-07 12:30:36 +0530

    adding new problem set and solved1 problem

---
+++ problem_set1/deep/deep.py
@@ -0,0 +1,11 @@
+q_life = str(input("What is the Answer to the Great Question of Life , the Universe, and Everything? "))
+question = q_life.strip().lower()
+
+if question == '42':
+    print("Yes")
+elif question == 'forty-two':
+    print("yes")
+elif question == 'forty two':
+    print("yes")
+else :
+    print("No")
