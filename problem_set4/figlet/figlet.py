113792aed881f70be324ca11972838ed89e516e6
cyberseekerx <kanekarayush777@proton.me>
2025-10-15 10:47:00 +0530

    s

--- problem_set4/figlet/figlet.py
+++ problem_set4/figlet/figlet.py
@@ -1,8 +1,26 @@
 import sys 
+import random
 from pyfiglet import Figlet 
-
-text = input("input:")
-f = Figlet()
-if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-    f.setFont(font= sys.argv[2])
-    print(f.renderText(text))
+sub_commands =["-f","--fonts"]
+figlet = Figlet()
+if len(sys.argv) == 1 :
+    font =random.choice(figlet.getFonts())
+    figlet.setFont(font=font)
+    text = input("input: ")
+    print("Output:")
+    print(figlet.renderText(text))
+    sys.exit()
+#take user input
+elif len(sys.argv) == 3 and sys.argv[1] in sub_commands:
+    print(sys.argv[1])
+#check if the font is avaliable from sys.argv[2]
+    if sys.argv[2] in figlet.getFonts():
+        figlet.setFont(font=sys.argv[2])
+#render the input
+        text = input("input: ")
+        print("Output:")
+        print(figlet.renderText(text))
+    else:
+        sys.exit("Invalid usage")
+else:
+    sys.exit("Invalid usage")
