f3f82734cca22751e241d4d8a8cd7f4d3d2dc392
cyberseekerx <kanekarayush777@proton.me>
2025-10-20 09:02:50 +0530

    print debug for the level 1

--- problem_set4/professor/professor.py
+++ problem_set4/professor/professor.py
@@ -19,29 +19,43 @@ def generate_integer(lev):
     correct = 0
     tries = 0
     if lev == 1:
-        for que in range(11):
+        for que in range(10):
             x = random.randint(0,9)
             y = random.randint(0,9)
-            z = y + x
-            ans = get_int(x,y)
-            if ans == z:
-                correct = correct + 1
-                tries= 0
-            elif tries == 3:
-                print(f"{x} + {y} = {z}")
-                que += 1
-            else:
-                print("EEE")
-                continue
-
+            while True:
+                z = y + x
+                try:
+                    if tries == 3:
+                        print(f"{x} + {y} = {z}")
+                        print('que',que)
+                        que += 1
+                        tries = 0
+                        print('que',que)
+                        break
+                    ans = int(input(f"{x} + {y} = "))
+                    if ans == z:
+                        correct = correct + 1
+                        que += 1
+                        tries= 0
+                        print("tries",tries)
+                        break
+                    else:
+                        tries += 1
+                        print('tries',tries)
+                        print("EEE")
+                        continue
+                except ValueError:
+                    tries += 1
+    print("que",que)
 
 def get_int(x,y):
+    #To take input from the user about the answer
     tr = 0
     while True:
         try:
             if tr == 3:
                 break
-            answer = int(input(f"{x} + {y} = "))
+            answer 
             return answer
         except ValueError:
             tr += 1
